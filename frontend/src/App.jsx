8ce66423cab07391e1a665305f3924834b635939
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2025-02-22 15:31:01 +0530

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import { EmployeeManagement } from "./components/EmployeeManagement";
 import Header from "./components/Header";
 import { TaskManagement } from "./components/TaskManagement";
+import { TaskBoard } from "./components/EmployeeTabs";
 
 function App() {
 
   const [employees, setEmployees] = useState([]);
+  const [taskList, setTaskList] = useState([]);
   const [error, setError] = useState("");
   
   useEffect(() => {
@@ -16,7 +18,22 @@ function App() {
     })
     .then((data) => {
       setEmployees(data);
-      console.log("Emp data :",data);
+      // console.log("Emp data :",data);
+    })
+    .catch((error) => {
+      setError(error.message);
+    })
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:5500/api/task/getTaskList")
+    .then((response) => {
+      if(!response.ok) throw new Error ("Failed to fetch employees...")
+        return response.json();
+    })
+    .then((data) => {
+      setTaskList(data);
+      // console.log("Emp data :",data);
     })
     .catch((error) => {
       setError(error.message);
@@ -24,11 +41,16 @@ function App() {
   }, []);
 
   return (
-    <div id="container" className="bg-gray-100 h-screen">
+    <div id="container" className="bg-gray-100">
       <Header/>
-      <div className="w-10/12 m-auto flex justify-between">
-        <EmployeeManagement/>
-        <TaskManagement employees={employees}/>
+      <div className="w-10/12 m-auto">
+        
+        <main className="flex justify-between">
+          <EmployeeManagement/>
+          <TaskManagement employees={employees}/>
+        </main>
+
+        <TaskBoard employees={employees} taskList={taskList}/>
       </div>
     </div>
   )
--- frontend/src/components/TaskManagement.jsx
+++ frontend/src/components/TaskManagement.jsx
@@ -1,4 +1,40 @@
+import axios from "axios";
+import { useState } from "react"
+
 export const TaskManagement = ({employees}) => {
+
+    const [taskTitle, setTaskTitle] = useState("");
+    const [taskDesc, setTaskDesc] = useState("");
+    const [assignedEmp, setAssignedEmp] = useState("");
+    const [suggestions, setSuggestions] = useState([]);
+    const [predictingTime, setPredictingTime] = useState(null);
+
+    // Auto-completion suggestions API
+    const handleTaskSuggestion = async (e) => {
+        // setTaskTitle(e.target.value);
+        if(taskTitle.length > 3) {
+            try {   
+                const response = await axios.post("http://localhost:5500/api/task/suggest", {input: taskTitle});
+                setSuggestions(response.data.suggestions.split("\n"));
+                console.log("Suggestions :",suggestions);
+            } catch (error) {
+                console.log("Error while fetching suggestions :",error);
+            }
+        }
+    }
+
+    const handleAssignTask = async (e) => {
+        try {
+            const res = await axios.post("http://localhost:5500/api/task/createTask", {taskTitle, taskDesc, assignedEmp});
+            setTaskTitle("");
+            setTaskDesc("");
+            setAssignedEmp("");
+            setSuggestions([])
+        } catch (error) {
+            console.error("Error creating task :",error);
+        }
+    }
+
     return (
         <div className="task-wrapper bg-white shadow-md rounded p-5 w-7/12">
             <h2 className="text-3xl text-center font-semibold mb-5">Assign Task</h2>
@@ -9,7 +45,24 @@ export const TaskManagement = ({employees}) => {
                     type="text"
                     placeholder="Enter Task Title"
                     className="border w-full p-2"
+                    value={taskTitle}
+                    onChange={(e) => setTaskTitle(e.target.value)}
                 />
+
+                {
+                    suggestions.length > 0 && (
+                        <ul>
+                            {
+                                suggestions.map((s, i) => (
+                                    <li key={i}>
+                                        {s}
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    )
+                }
+
             </div>
 
             <div className="input-group mb-4">
@@ -17,12 +70,16 @@ export const TaskManagement = ({employees}) => {
                 <textarea
                     placeholder="Enter Task Description"
                     className="border w-full p-2"
+                    value={taskDesc}
+                    onChange={(e) => setTaskDesc(e.target.value)}
                 />
             </div>
 
             <div className="input-group mb-4">
                 <label className="block mb-2">Select Emp</label>
-                <select className="border w-full p-2">
+                <select 
+                onChange={(e) => setAssignedEmp(e.target.value)}
+                className="border w-full p-2">
                     <option>Select Employee</option>
                     {
                         employees.map((emp) => (
@@ -36,9 +93,17 @@ export const TaskManagement = ({employees}) => {
 
             <div className="btn-group text-center">
                 <button 
+                    onClick={handleAssignTask}
                     className="w-1/2 bg-indigo-500 text-white py-3"
                 >
                     Assign Task
+                </button>
+
+                <button 
+                onClick={handleTaskSuggestion}
+                    className="w-1/3 bg-indigo-500 text-white py-3 ml-2"
+                >
+                    Generate AI Task Suggestions
                 </button>
             </div>
         </div>
