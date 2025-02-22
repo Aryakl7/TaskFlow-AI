a7c1f2c048d5bded4a2c14f0caf65dac1e1b6ecd
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2025-02-22 15:30:12 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -5,19 +6,18 @@ const cors = require("cors");
 const app = express();
 
 const empRoute = require("./routes/empRoute");
-
-const MONGO_URI=`mongodb+srv://admin:admin@cluster0.zxvoo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const taskRoute = require("./routes/taskRoute");
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MONGODB Connected"))
 .catch(err => console.error(err));
 
 
 app.use("/api/emp", empRoute);
-// app.use("/api/task", taskRoutes);
+app.use("/api/task", taskRoute);
 
 const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => console.log("Server running on PORT",PORT));
